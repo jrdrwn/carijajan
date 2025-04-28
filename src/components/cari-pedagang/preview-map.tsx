31e8a0e69f3d7e18e4be34d801fe2f9cdca6d1e1
Jordi Irawan <jordirwn@gmail.com>
2025-04-28 15:45:25 +0700

    refactor: add z-index to MapContainer for improved layering in PreviewMap component

--- src/components/cari-pedagang/preview-map.tsx
+++ src/components/cari-pedagang/preview-map.tsx
@@ -22,7 +22,7 @@ interface MapProps {
 
 export default function PreviewMap({ center, position }: MapProps) {
   return (
-    <MapContainer center={center} zoom={20} className="size-full rounded-xl">
+    <MapContainer center={center} zoom={20} className="size-full rounded-xl z-0">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
