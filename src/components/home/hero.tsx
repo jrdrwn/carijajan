0912c5af578f0de9107676994cf10e7dbfce79bc
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2025-04-29 07:50:19 +0700

    refactor: enhance responsiveness and adjust styling in Hero component

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -39,35 +39,35 @@ export default function Hero() {
   };
 
   return (
-    <section className="container mx-auto flex flex-col justify-center px-2 py-16 lg:flex-row lg:justify-between lg:px-0">
+    <section className="container mx-auto flex flex-col justify-center px-2 py-16 lg:flex-row lg:justify-between">
       <div className="text-center lg:text-start">
-        <div className="mx-auto flex w-max items-center gap-2 rounded-full bg-muted px-2 py-1 text-muted-foreground lg:mx-0 lg:gap-4 lg:px-4 lg:py-2">
+        <div className="mx-auto flex w-max items-center gap-2 rounded-full bg-muted px-2 py-1 text-muted-foreground lg:mx-0 xl:gap-4 xl:px-4 xl:py-2">
           <span>
-            <BellDot className="size-5 text-primary lg:size-6" />
+            <BellDot className="size-5 text-primary xl:size-6" />
           </span>
-          <p className="text-sm lg:text-base">
+          <p className="text-sm xl:text-base">
             30+ Pedagang Baru Telah Ditambahkan!
           </p>
           <span className="rounded-full bg-neutral-500 p-1">
-            <ArrowRight className="size-5 text-neutral-50 lg:size-6" />
+            <ArrowRight className="size-5 text-neutral-50 xl:size-6" />
           </span>
         </div>
-        <h1 className="mt-4 mb-4 max-w-3xl bg-gradient-to-br from-orange-900 to-neutral-500 bg-clip-text text-5xl leading-tight font-bold tracking-wide text-transparent lg:mb-8 lg:text-7xl">
+        <h1 className="mt-4 mb-4 max-w-3xl bg-gradient-to-br from-orange-900 to-neutral-500 bg-clip-text text-4xl leading-tight font-bold xl:tracking-wide text-transparent lg:mb-6 xl:mb-8 xl:text-6xl 2xl:text-7xl">
           Temukan Pedagang Kaki Lima Terdekat di Sekitarmu!
         </h1>
-        <p className="max-w-2xl leading-relaxed tracking-wide lg:text-lg">
+        <p className="max-w-2xl leading-relaxed xl:tracking-wide xl:text-lg">
           Jelajahi peta untuk menemukan makanan kaki lima favoritmu mulai dari
           bakso keliling, sate gerobak, hingga es doger yang legendaris!
         </p>
-        <div className="mt-8 flex items-center justify-center gap-4 lg:mt-12 lg:justify-start">
+        <div className="mt-8 flex items-center justify-center gap-4 lg:justify-start 2xl:mt-12">
           <Input
             type="text"
             placeholder="Contoh: Gorengan Renyah..."
-            className="w-full max-w-3xs placeholder:text-sm lg:max-w-[400px] lg:px-4 lg:py-6 lg:placeholder:text-base"
+            className="w-full max-w-3xs placeholder:text-sm lg:max-w-[350px] lg:px-3 lg:py-5 xl:max-w-[400px] xl:px-4 xl:py-6 xl:placeholder:text-base"
             onChange={(e) => setQ(e.currentTarget.value)}
             value={q}
           />
-          <Button className="lg:px-4 lg:py-6" asChild>
+          <Button className="lg:px-3 lg:py-5 xl:px-4 xl:py-6" asChild>
             <Link
               scroll={false}
               href={{
@@ -76,11 +76,11 @@ export default function Hero() {
               }}
             >
               {<Search />}
-              <span className="hidden lg:block">Cari Pedagang</span>
+              <span className="hidden xl:block">Cari Pedagang</span>
             </Link>
           </Button>
         </div>
-        <div className="mx-auto mt-8 flex w-fit flex-wrap items-center justify-center gap-2 lg:mx-0 lg:mt-12">
+        <div className="mx-auto mt-8 flex w-fit flex-wrap items-center justify-center gap-2 lg:mx-0 2xl:mt-12">
           <span className="mr-2 inline-flex items-center -space-x-4">
             {reviews.avatars.map((avatar, index) => (
               <Avatar key={index} className="size-12 border-2 border-white">
@@ -113,35 +113,35 @@ export default function Hero() {
           </div>
         </div>
       </div>
-      <div className="w-full pt-20 lg:w-1/2 lg:pt-0">
-        <div className="relative mx-auto flex items-center justify-center lg:h-170 lg:w-200">
+      <div className="w-full pt-20 lg:max-w-lg xl:max-w-xl xl:pt-0 2xl:max-w-3xl">
+        <div className="relative mx-auto flex items-center justify-center lg:h-65 lg:w-120 xl:h-140 2xl:h-170 2xl:w-180">
           <Image
             src={'https://images.unsplash.com/photo-1572000423136-e94e163fb50b'}
             alt="hero"
             width={800}
             height={800}
-            className="absolute -top-10 right-0 h-20 w-40 rounded-lg object-cover object-center shadow-lg lg:top-15 lg:right-10 lg:h-40 lg:w-60"
+            className="absolute -top-10 right-0 h-20 w-40 rounded-lg object-cover object-center shadow-lg lg:-top-10 lg:right-5 xl:top-5 xl:h-30 xl:w-50 2xl:top-10 2xl:right-5 2xl:h-40 2xl:w-60"
           />
           <Image
             src={'https://images.unsplash.com/photo-1597247003506-42ee1e7bc7dd'}
             alt="hero"
             width={800}
             height={800}
-            className="absolute bottom-25 left-0 h-20 w-40 rounded-lg object-cover object-top shadow-lg lg:bottom-60 lg:left-15 lg:h-25 lg:w-50"
+            className="absolute bottom-25 left-0 h-20 w-40 rounded-lg object-cover object-top shadow-lg lg:bottom-25 lg:left-10 xl:bottom-60 xl:left-5 xl:h-25 xl:w-50"
           />
           <Image
             src={'https://images.unsplash.com/photo-1514425263458-109317cc1321'}
             alt="hero"
             width={800}
             height={800}
-            className="absolute right-0 -bottom-5 h-10 w-60 rounded-lg object-cover object-center shadow-lg lg:right-auto lg:bottom-15 lg:h-15 lg:w-85"
+            className="absolute right-0 -bottom-5 h-10 w-60 rounded-lg object-cover object-center shadow-lg lg:right-auto lg:-bottom-7 xl:bottom-15 xl:h-15 xl:w-85"
           />
           <Image
             src={'https://images.unsplash.com/photo-1733509524469-12fb3d629b7a'}
             alt="hero"
             width={800}
             height={800}
-            className="h-70 w-70 rounded-lg object-cover object-center lg:h-125 lg:w-125"
+            className="h-70 w-70 rounded-lg object-cover object-center xl:h-100 xl:w-100 2xl:h-125 2xl:w-125"
           />
         </div>
       </div>
